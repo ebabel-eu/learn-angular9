bc5cfc001387245b9dd158289b9ec3c5f7b83e8d
Thomas Amar <hello@ebabel.eu>
2020-04-24 16:41:41 +0200

    Fix unit test.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('To Angular 9 and beyond!');
   });
 });
